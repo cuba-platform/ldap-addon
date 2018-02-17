c3ddef3706c2c7da0d489b445c7bc8a358a8e96e
test <test@example.com>
2018-02-17 22:17:45 +0400

    https://youtrack.haulmont.com/issue/LDAPC-48

---
+++ modules/core/db/update/postgres/18/180217-99-insertFixedMatchingRule.sql
@@ -0,0 +1,3 @@
+INSERT INTO LDAP_MATCHING_RULE (id,version,create_ts,created_by,rule_type,description,is_terminal_rule,is_disabled,is_override_existing_roles,is_override_exist_access_grp) 
+values
+('ff2ebe74-3836-465b-9185-60141a6a0548',0,now(),'admin','FIXED','Fixed rule',false,false,false,false);
