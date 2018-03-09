ba7a4a6746d8a7b0e6efc8e86987af5c0e0d4f03
test <test@example.com>
2018-03-09 23:10:59 +0400

    https://youtrack.haulmont.com/issue/LDAPC-74

--- modules/web/src/com/haulmont/addon/ldap/web/simplerulecondition/SimpleRuleConditionEdit.java
+++ modules/web/src/com/haulmont/addon/ldap/web/simplerulecondition/SimpleRuleConditionEdit.java
@@ -1,7 +1,25 @@
 package com.haulmont.addon.ldap.web.simplerulecondition;
 
+import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.gui.components.AbstractEditor;
 import com.haulmont.addon.ldap.entity.SimpleRuleCondition;
+import com.haulmont.cuba.gui.data.Datasource;
+
+import javax.inject.Named;
 
 public class SimpleRuleConditionEdit extends AbstractEditor<SimpleRuleCondition> {
+
+
+    @Named("simpleRuleConditionDs")
+    private Datasource<SimpleRuleCondition> simpleRuleConditionDs;
+
+    @Override
+    public void setItem(Entity item) {
+        super.setItem(item);
+        SimpleRuleCondition inMemorySrc = (SimpleRuleCondition) item;
+        SimpleRuleCondition src = simpleRuleConditionDs.getItem();
+        src.setAttribute(inMemorySrc.getAttribute());
+        src.setAttributeValue(inMemorySrc.getAttributeValue());
+
+    }
 }
