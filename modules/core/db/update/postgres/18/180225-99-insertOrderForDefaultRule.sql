364865bdeaef52484f1e4b8d25b3e2c2cca7b247
antmarr <marin@haulmont.com>
2018-04-08 22:15:08 +0400

    https://github.com/cuba-platform/ldap-addon/issues/2 https://github.com/cuba-platform/ldap-addon/issues/1
    LDAP_Config is not created when launching the application for the first time
    
    Default rule is not created when launching the application for the first time

--- modules/core/db/update/postgres/18/180225-99-insertOrderForDefaultRule.sql
+++
@@ -1,2 +0,0 @@
-insert into LDAP_MATCHING_RULE_ORDER(id,ORDER_,version,create_ts,created_by) values('ff2ebe74-3836-465b-9185-60141a6a0548',2147483647,0,now(),'admin');
-update LDAP_MATCHING_RULE set MATCHING_RULE_ORDER_ID = 'ff2ebe74-3836-465b-9185-60141a6a0548' where id='ff2ebe74-3836-465b-9185-60141a6a0548';
--- modules/core/db/update/postgres/18/180325-99-LdapConfig.sql
+++ modules/core/db/update/postgres/18/180325-99-LdapConfig.sql
@@ -1,5 +1,5 @@
 INSERT INTO LDAP_LDAP_CONFIG (id,version,update_ts,updated_by,SCHEMA_BASE,LDAP_USER_OBJECT_CLASSES,OBJECT_CLASS_PROPERTY_NAME,ATTRIBUTE_PROPERTY_NAMES,
-EMAIL_ATTRIBUTE,CN_ATTRIBUTE,SN_ATTRIBUTE,MEMBER_OF_ATTRIBUTE,POSITION_ATTRIBUTE,OU_ATTRIBUTE,LANGUAGE_ATTRIBUTE,INACTIVE_USER_ATTRIBUTE,USER_BASE)
+EMAIL_ATTRIBUTE,CN_ATTRIBUTE,SN_ATTRIBUTE,MEMBER_OF_ATTRIBUTE,POSITION_ATTRIBUTE,OU_ATTRIBUTE,LANGUAGE_ATTRIBUTE,INACTIVE_USER_ATTRIBUTE,USER_BASE,LOGIN_ATTRIBUTE)
 values
 ('ff2ebe74-3836-465b-9185-60141a6a0548',0,now(),'admin','CN=Schema,CN=Configuration','person;inetOrgPerson','CN','systemMustContain;systemMayContain;mayContain;MustContain',
-'mail','cn','sn','memberOf','employeeType','ou','preferredLanguage','userAccountControl','');
+'mail','cn','sn','memberOf','employeeType','ou','preferredLanguage','userAccountControl','','sAMAccountName');
