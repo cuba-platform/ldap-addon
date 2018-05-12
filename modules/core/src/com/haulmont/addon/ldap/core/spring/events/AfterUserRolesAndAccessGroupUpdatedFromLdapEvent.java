99c61ee56518669d55c265e57ebb29639e3f1a7f
antmarr <marin@haulmont.com>
2018-05-12 18:51:29 +0400

    31. Create UserActivatedFromLdapEvent reate new Spring event, which fired after user was activated from LDAP

---
+++ modules/core/src/com/haulmont/addon/ldap/core/spring/events/AfterUserRolesAndAccessGroupUpdatedFromLdapEvent.java
@@ -0,0 +1,31 @@
+package com.haulmont.addon.ldap.core.spring.events;
+
+import com.haulmont.addon.ldap.core.rule.LdapMatchingRuleContext;
+import com.haulmont.cuba.security.entity.User;
+import org.springframework.context.ApplicationEvent;
+
+public class AfterUserRolesAndAccessGroupUpdatedFromLdapEvent extends ApplicationEvent {
+
+    private final LdapMatchingRuleContext ldapMatchingRuleContext;
+    private final User cubaUser;
+    private final SynchronizationMode synchronizationMode;
+
+    public AfterUserRolesAndAccessGroupUpdatedFromLdapEvent(Object source, LdapMatchingRuleContext ldapMatchingRuleContext, User cubaUser, SynchronizationMode synchronizationMode) {
+        super(source);
+        this.ldapMatchingRuleContext = ldapMatchingRuleContext;
+        this.cubaUser = cubaUser;
+        this.synchronizationMode= synchronizationMode;
+    }
+
+    public LdapMatchingRuleContext getLdapMatchingRuleContext() {
+        return ldapMatchingRuleContext;
+    }
+
+    public User getCubaUser() {
+        return cubaUser;
+    }
+
+    public SynchronizationMode getSynchronizationMode() {
+        return synchronizationMode;
+    }
+}
